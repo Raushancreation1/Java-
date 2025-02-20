4fbe52c538e3f657e6d8874e10ad39e63d58cea1
raushan kumar <raushancreationofficial@gmail.com>
2025-02-20 22:09:13 +0530

    Add files via upload

---
+++ data_types/Sum_Two_Number.java
@@ -0,0 +1,10 @@
+package data_types;
+
+public class Sum_Two_Number {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
